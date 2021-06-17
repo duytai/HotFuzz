4c1f566fecd0bd137fbcb9ffd1f843829000c952
Tai D. Nguyen <duytai.cse@gmail.com>
2021-06-17 17:08:18 +0800

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
-# build dependencies
-# (cd verifier && make)
-# (cd nnf && pip3 install -r requirements.txt)
+build dependencies
+(cd verifier && make)
+(cd nnf && pip3 install -r requirements.txt)
 
 # download rustc compiler
 wget -O stage2.tar.gz https://www.dropbox.com/s/5g26h9qo2rswm7h/stage2.tar.gz?dl=1
